570da9d9b8b1c22491afd94b91dd33b06e5f0cfe
Subhamkrojha <subhamkumar51@gmail.com>
2021-09-28 03:07:27 +0530

    dll file

---
+++ FileChordgenerator/FileChordgenerator/FileChordgenerator/Class1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileChordgenerator
+{
+    public class Class1
+    {
+        public enum FileType
+        {
+            Chords,
+            Tab,
+            Lyrics
+        }
+        public string SongName { get; set; }
+        public string Artist { get; set; }
+        public FileType fileType { get; set; }
+       
+
+        FileType filetype;
+
+
+       
+        public void getdata()
+        {
+
+          
+            List<string> lines = new List<string>();
+            //SongName = s.ToString();
+            //Artist = s.ToString();
+            for (; ; )
+            {
+                Console.WriteLine("Enter the input type\n'0' if you are entering chord\n'1' if you are entering a tab\n'2' if you are entering the lyrics\nEnter any other number to end");
+               
+                int ch = Convert.ToInt32(Console.ReadLine());
+
+                if (ch == 0)
+                    fileType = FileType.Chords;
+                else if (ch == 1)
+                    fileType = FileType.Tab;
+                else if (ch == 2)
+                    fileType = FileType.Lyrics;
+                else
+                {
+                    Console.WriteLine("Wrong selection");
+                    break;
+                }
+                filetype = fileType;
+                switch (filetype)
+                {
+                    case FileType.Chords:
+                        {
+                            Console.WriteLine("Enter the chord line");
+                            string str = Console.ReadLine();
+                            lines.Add("Chord: " + str);
+                            break;
+                        }
+                    case FileType.Tab:
+                        {
+                            Console.WriteLine("You have selected tab");
+                            string str = Console.ReadLine();
+                            lines.Add("     Tab      ");
+                            break;
+                        }
+                    case FileType.Lyrics:
+                        {
+                            Console.WriteLine("Enter the lyric line");
+                            string str = Console.ReadLine();
+                            lines.Add("Lyric: " + str);
+                            break;
+                        }
+                }
+            }
+            FileStream fs = new FileStream(@"C:\Users\Subham Kumar Ojha\Desktop\FileChordgenerator\test.txt", FileMode.Create, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+            sw.WriteLine("Songname: " + SongName);
+            sw.WriteLine("Artist: " + Artist);
+            foreach (string str in lines)
+            {
+                sw.WriteLine(str);
+            }
+            sw.Close();
+            fs.Close();
+        }
+    }
+}
---
+++ FileChordgenerator/FileChordgenerator/FileChordgenerator/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FileChordgenerator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FileChordgenerator")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("26634234-c74c-402e-94d5-2d7418ddf7ff")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
